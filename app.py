19319b88159c72f95ed545a55c4819911d330b73
Rajesh-hugar <rajeshhugar220@gmail.com>
2023-04-29 12:18:31 +0530

    changes done

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ similarity = pickle.loads(bz2.decompress(ifile.read()))
 ifile.close()
 
 
+
+
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
     data = response.json()
